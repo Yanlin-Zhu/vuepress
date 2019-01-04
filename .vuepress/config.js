193b2936cb5dac15417eead9104d13aedf1a3ecb
朱彦霖 <zhuyanlin@renrenche.com>
2019-01-04 11:42:40 +0800

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'zz',
+  description: 'zz',
+  head: [
+      ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `https://assets-cdn.github.com/favicon.ico` }]
+  ],
+  base: '/',
+  host: '127.0.0.1',
+  port: 8080,
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'zz', link: '/zz' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      {
+        title: 'Group 1',
+        collapsable: false,
+        children: [
+          '/'
+        ]
+      },
+      {
+        title: 'Group 2',
+        children: [ 
+          '/zz'
+        ]
+      }
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+
+# Hello VuePress
+::: tip
+This is a tip !
+:::
+
+::: warning
+This is a warning !
+:::
+
+::: danger
+This is a dangerous warning !
+:::
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+:tada: :100:
+
+## zz
+
+## bb
